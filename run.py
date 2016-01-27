fb9c40f86fc98a9c11410d940d5c8a2d57b2b229
Shraya Ramani <shraya.ramani@10gen.com>
2016-01-27 11:43:27 -0500

    changed something to fail instead of pass

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#/usr/bin/env python
 import time
 import sys
 import os
@@ -38,7 +38,7 @@ def main(args):
                         "end":1398782500.681
                         },
                     {
-                        "status":"pass",
+                        "status":"fail",
                         "test_file":"test_2",
                         "url":"http://buildlogs.mongodb.org/build/535fba25d2a60f3135000ae7/test/535fba25d2a60f320c000127/",
                         "exit_code":0,
